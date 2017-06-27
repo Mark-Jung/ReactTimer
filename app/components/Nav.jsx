60fdf8184e5f37162c61e8258a608a82ddf904e2
Mark Jung <gujung2022@u.northwestern.edu>
2017-06-27 14:56:55 +0900

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,45 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+
+
+var Nav = React.createClass({
+  render: function() {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Timer App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+            </li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Countdown</IndexLink>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">
+              <p>Created by <p className="credit">Mark Jung</p></p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = Nav;
