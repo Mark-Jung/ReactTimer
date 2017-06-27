42a4116df589d10be5f47aaece5c230e20e60e6c
Mark Jung <gujung2022@u.northwestern.edu>
2017-06-27 22:06:27 +0900

    added clock css and tests

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,9 @@ module.exports ={
       applicationStyles: 'app/styles/app.scss',
       Nav: 'app/components/Nav.jsx',
       Timer: 'app/components/Timer.jsx',
-      Countdown: 'app/components/Countdown.jsx'
+      Countdown: 'app/components/Countdown.jsx',
+      Clock: 'app/components/Clock.jsx',
+      CountdownForm: 'app/components/CountdownForm.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
